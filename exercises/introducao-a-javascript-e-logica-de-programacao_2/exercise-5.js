9919d376c35c0854c20a31965f1188928b035e66
Douglas <douguinhodns@gmail.com>
2021-02-25 20:22:59 -0300

    exercise-5 concluido

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise-5.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+var resultado = 0;
+
+for (index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] > resultado) {
+        resultado = numbers[index];
+    }
+}
+
+console.log(resultado);
