09ca44caaf5e3a343fe2dd04228c5ae29ca99821
Douglas <douguinhodns@gmail.com>
2021-02-25 21:03:47 -0300

    incluido let no index do for

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise-5.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise-5.js
@@ -2,7 +2,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 var resultado = 0;
 
-for (index = 0; index < numbers.length; index += 1) {
+for (let index = 0; index < numbers.length; index += 1) {
     if (numbers[index] > resultado) {
         resultado = numbers[index];
     }
