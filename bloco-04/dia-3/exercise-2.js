777759c803a5a564b12d4ae1ff3998862dc9d578
Douglas <douguinhodns@gmail.com>
2021-02-27 11:15:30 -0300

    exercise-2.js concluido

---
+++ bloco-04/dia-3/exercise-2.js
@@ -0,0 +1,18 @@
+// criar variável n
+// imprimir '*' 1 a mais a cada linha até a linha n
+// criar uma variavel para ser usada com valor '*'
+// for vai decidir quantos '*' quero por linha
+// if vai ser a condição de parada ou mostra resultado por linha no console
+
+n = 5;
+
+let resultado = '*';
+
+    console.log(resultado);
+    for (index = 1; index < n; index += 1) {
+        if (index >= n) {
+            break;
+        } else {
+            console.log(resultado += '*')
+        }
+    }
